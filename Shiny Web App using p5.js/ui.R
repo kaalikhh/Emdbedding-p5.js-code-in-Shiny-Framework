5fec783a670e4ba49cef887d5c12e4254e0a0470
Saurabh Yadav <saurabhy789@gmail.com>
2019-06-03 13:41:02 +0530

    Update ui.R

--- Shiny Web App using p5.js/ui.R
+++ Shiny Web App using p5.js/ui.R
@@ -15,8 +15,10 @@ shinyUI(
   fluidRow(
     # THE UI
     tags$html(
-      tags$body())
-    
-    
-    
+      tags$body(        
+        singleton(tags$head(tags$script(src = "p5.min.js")))
+        ,singleton(tags$head(tags$script(src = "sketch1.js")))
+        ,singleton(tags$div(id = 'divCanvas', style = 'width:auto; height:auto'))
     ))
+)
+)
